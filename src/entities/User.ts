5e6b9662e8ddc16797da686fda32abb27d506fe4
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-03-04 15:47:56 -0300

    ajusta tipos de coluna para propriedades do usuário

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -7,13 +7,13 @@ export class User {
     @PrimaryGeneratedColumn('uuid')
     id!: string;
 
-    @Column()
+    @Column({ type: 'varchar' })
     name!: string;
 
-    @Column({ unique: true })
+    @Column({ type: 'varchar', unique: true })
     email!: string;
 
-    @Column()
+    @Column({ type: 'varchar' })
     password!: string;
 
     @CreateDateColumn()
